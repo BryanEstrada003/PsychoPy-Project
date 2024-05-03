a2fd60cc1b6033b867c3d968b66d4f5fc871c452
BryanEstrada003 <mbestrad@espol.edu.ec>
2024-05-03 01:42:54 -0500

    Modify css style main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Chat from './components/Chat.jsx'
+import React from 'react';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <h1>Text Prompt Demo</h1>
-      <Chat />
-    </>
+    <div className="container">
+      <div id="div1">
+        <h1>PsychoPy</h1>
+        <Chat />
+      </div>
+      <div id="div2">
+        <p>esto sera el chat</p>
+      </div>
+    </div>
   );
 }
 
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,22 +1,59 @@
 import React, { useState } from 'react';
+import './Chat.css';
 
 const Chat = () => {
     const [input, setInput] = useState('');
+    const [input2, setInput2] = useState('');
+    const [input3, setInput3] = useState('');
+    const [input4, setInput4] = useState('');
     const [output, setOutput] = useState('');
   
     const handleInputChange = (e) => {
       setInput(e.target.value);
     };
-  
+
+    const handleInputChange2 = (e) => {
+        setInput2(e.target.value);
+    };
+
+    const handleInputChange3 = (e) => {
+        setInput3(e.target.value);
+    };
+
+    const handleInputChange4 = (e) => {
+        setInput4(e.target.value);
+    };
+
     const handleSubmit = async () => {
       // Aquí invocarás la API de generación de texto
     };
   
     return (
       <div>
-        <input type="text" value={input} placeholder="Enter your prompt here..." onChange={handleInputChange} />
-        <button onClick={handleSubmit}>Generar</button>
-        {output && <p>{output}</p>}
+        <div className="input-container">
+            <input type="text" value={input} placeholder="Enter your prompt here..." onChange={handleInputChange} />
+            <button className="button" onClick={handleSubmit}>Generar</button>
+            {output && <p>{output}</p>}
+        </div>
+
+
+        <div className="input-container">
+            <input type="text" value={input2} placeholder="Enter your prompt here..." onChange={handleInputChange2} />
+            <button className="button" onClick={handleSubmit}>Generar</button>
+            {output && <p>{output}</p>}
+        </div>
+
+        <div className="input-container">
+            <input type="text" value={input3} placeholder="Enter your prompt here..." onChange={handleInputChange3} />
+            <button className="button" onClick={handleSubmit}>Generar</button>
+            {output && <p>{output}</p>}
+        </div>
+
+        <div className="input-container">
+            <input type="text" value={input4} placeholder="Enter your prompt here..." onChange={handleInputChange4} />
+            <button className="button" onClick={handleSubmit}>Generar</button>
+            {output && <p>{output}</p>}
+        </div>
       </div>
     );
   };
