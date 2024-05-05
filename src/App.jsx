e1096bef3ff0151d374f1ff1318d0040ec47ebdb
BryanEstrada003 <mbestrad@espol.edu.ec>
2024-05-04 20:48:40 -0500

    Modify light dark

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import React, { useState } from 'react';
 import './App.css';
 import nightModeIcon from './assets/night-mode.png';
 import { GoogleGenerativeAI } from '@google/generative-ai';
-
-
+import './index.css';
 
 function App() {
   // light dark mode
@@ -16,21 +15,20 @@ function App() {
 
   const genAI = new GoogleGenerativeAI(apiKey);
 
-
   const model = genAI.getGenerativeModel({model: "gemini-1.0-pro"})
 
   console.log("apiKey", apiKey)
 
   return (
     <div className={`container ${darkMode ? 'dark' : ''}`}>
-      <div id="input">
+      <div id="input" className={`${darkMode ? 'dark' : ''}`}>
         <h1>PsychoPy</h1>
         <Chat />
         <button onClick={() => setDarkMode(!darkMode)}>
           <img src={nightModeIcon} alt="Night mode" />
         </button>
       </div>
-      <div id="current-chat">
+      <div id="current-chat" className={`${darkMode ? 'dark' : ''}`}>
         <p>esto sera el chat</p>
       </div>
     </div>
